bd02413f9aeaa6d4d811c10484798d0599cc8a23
Patrick Bird <p_h_bird@yahoo.fr>
2023-05-25 15:47:08 +0200

    add movie model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'movies/title:text'
-  get 'movies/overview:text'
-  get 'movies/poster_url:text'
-  get 'movies/rating:float'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

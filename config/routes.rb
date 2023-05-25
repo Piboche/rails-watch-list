f29c3562fd0578a4736354f79d6f48bed206ee6c
Patrick Bird <p_h_bird@yahoo.fr>
2023-05-25 15:04:59 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'movies/title:text'
+  get 'movies/overview:text'
+  get 'movies/poster_url:text'
+  get 'movies/rating:float'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
